0ceb4d0ecb73d391cbe6fdbe469e147fafd4d4c9
Ayush-Dhawan <ayushdhawan102004@gmail.com>
2024-09-07 12:13:16 +0530

    servlet chaining

---
+++ src/main/java/com/studentDB/DB_CONNECT.java
@@ -0,0 +1,27 @@
+
+package com.studentDB;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DB_CONNECT {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/students";
+    private static final String USER = "root";
+    private static final String PASSWORD = "ayush@1234";
+    private static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+
+    static {
+        try {
+            Class.forName(JDBC_DRIVER);
+            System.out.println("JDBC Driver loaded successfully.");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("JDBC Driver class not found: " + e.getMessage(), e);
+        }
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
---
+++ src/main/java/com/studentDB/Student.java
@@ -0,0 +1,11 @@
+package com.studentDB;
+
+public class Student{
+    public int roll_number;
+    public String name;
+
+    public Student(int roll_number, String name) {
+        this.roll_number = roll_number;
+        this.name = name;
+    }
+}
